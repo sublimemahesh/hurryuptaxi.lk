d8821c13309516dfd9c2f147fdfb9b1f208dc82d
mayomi <mayomi@sublime.lk>
2018-05-04 14:48:35 +0530

    #43 Index page

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <title>Welcome || Sublime Web Manager</title>
+        <!-- Favicon-->
+        <link rel="icon" href="member/favicon.ico" type="image/x-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+        <!-- Bootstrap Core Css -->
+        <link href="member/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+        <!-- Waves Effect Css -->
+        <link href="member/plugins/node-waves/waves.css" rel="stylesheet" />
+
+        <!-- Animation Css -->
+        <link href="member/plugins/animate-css/animate.css" rel="stylesheet" />
+
+        <!-- Custom Css -->
+        <link href="member/css/style.css" rel="stylesheet">
+        <style>
+            .btn-index{
+                text-align: center;
+                color: #fff !important;
+                background-color: #00BCD4;
+                border-radius: 12px;
+                padding: 6px;
+            }
+        </style>
+    </head> 
+
+    <body class="login-page">
+        <div class="login-box">
+            <div class="logo">
+                <a href="javascript:void(0);"><b>Control Panel</b></a>
+                <small>Support Lanka</small>
+            </div>
+            <div class="card">
+                <div class="body">
+                    <a href="member/login.php" class="btn-index center-block"><b>LOGIN</b></a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Jquery Core Js -->
+        <script src="member/plugins/jquery/jquery.min.js"></script>
+
+        <!-- Bootstrap Core Js -->
+        <script src="member/plugins/bootstrap/js/bootstrap.js"></script>
+
+        <!-- Waves Effect Plugin Js -->
+        <script src="member/plugins/node-waves/waves.js"></script>
+
+        <!-- Validation Plugin Js -->
+        <script src="member/plugins/jquery-validation/jquery.validate.js"></script>
+
+        <!-- Custom Js -->
+        <script src="member/js/admin.js"></script>
+        <script src="member/js/pages/examples/sign-in.js"></script>
+    </body>
+
+</html>
