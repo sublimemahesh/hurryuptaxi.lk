f2c415503419ef164c9268801e981ed33d8e115d
KNisansala <kavini@sublime.lk>
2018-06-05 17:14:40 +0530

    #190 Home Page Slider Changes

--- slider.php
+++ slider.php
@@ -1,14 +1,12 @@
 <div class="slider-wrapper theme-default">
     <div id="slider" class="nivoSlider">
-        <img src="assets/images/slider/slide_1.jpg" title="#htmlcaption1" data-thumb="assets/images/slider/slide_1.jpg" alt="" />
-<!--        <img src="assets/images/slider/slide_2.jpg" title="#htmlcaption2" data-thumb="assets/images/slider/slide_2.jpg" alt="" />
-        <img src="assets/images/slider/slide_3.jpg" title="#htmlcaption3" data-thumb="assets/images/slider/slide_3.jpg" alt="" />-->
+        <img src="assets/images/slider/02.jpg" title="#htmlcaption1" data-thumb="assets/images/slider/slide_1.jpg" alt="" />
     </div>
     <div id="htmlcaption1" class="nivo-caption">
         <div class="nivo-caption__inner">
             <div class="text">
-                <h1 class="showtext showtext__h1">Trusted <br>by 5 Million<br> Customers<br> &amp; Counting</h1>
-                <span class="showtext showtext__link"><a href="#">Start your travel planning here!</a></span>
+                <h1 class="showtext showtext__h1">Enjoy <br/>the efficient and specialized<br/> services of<br/> HURRYUP TAXI</h1>
+                
             </div>
         </div>
     </div>
